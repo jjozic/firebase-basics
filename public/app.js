1625f9fd817cbc4f991fa4a24e271837e757dadd
Julian Jozic <jjozic98@gmail.com>
2020-08-04 13:19:17 +0200

    small refactor

--- public/app.js
+++ public/app.js
@@ -29,12 +29,10 @@ auth.onAuthStateChanged(user => {
 });
 
 ///// Firestore /////
-
 const db = firebase.firestore();
 
 const createThing = document.getElementById('createThing');
 const thingsList = document.getElementById('thingsList');
-
 
 let thingsRef;
 let unsubscribe;
@@ -42,12 +40,10 @@ let unsubscribe;
 auth.onAuthStateChanged(user => {
 
     if (user) {
-
         // Database Reference
         thingsRef = db.collection('things')
 
         createThing.onclick = () => {
-
             const { serverTimestamp } = firebase.firestore.FieldValue;
 
             thingsRef.add({
@@ -56,28 +52,19 @@ auth.onAuthStateChanged(user => {
                 createdAt: serverTimestamp()
             });
         }
-
-
         // Query
         unsubscribe = thingsRef
             .where('uid', '==', user.uid)
             .orderBy('createdAt') // Requires a query
             .onSnapshot(querySnapshot => {
-                
                 // Map results to an array of li elements
-
                 const items = querySnapshot.docs.map(doc => {
 
                     return `<li>${doc.data().name}</li>`
 
                 });
-
                 thingsList.innerHTML = items.join('');
-
             });
-
-
-
     } else {
         // Unsubscribe when the user signs out
         unsubscribe && unsubscribe();
